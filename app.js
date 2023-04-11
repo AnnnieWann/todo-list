f40a308fb975d73e04b98f0a6e5750736169795b
Annie <annie_wan1209@hotmail.com>
2023-04-11 11:06:49 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const Todo = require("./models/todo");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -27,7 +28,10 @@ db.once("open", () => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Todo.find()
+    .lean()
+    .then((todos) => res.render("index", { todos }))
+    .catch((error) => console.error(error));
 });
 
 app.listen(3000, () => {
